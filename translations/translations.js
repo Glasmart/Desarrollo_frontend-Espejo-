4eba3e56106664787eb4d86657412405295e4334
uRicardoCaballero <r.ca.gtz@gmail.com>
2020-11-01 15:48:22 -0800

    agregado el idioma espa;ol e ingles

---
+++ translations/translations.js
@@ -0,0 +1,9 @@
+
+var translations = {
+	en: "translations/en.json", // English
+	es: "translations/es.json", // Spanish
+};
+
+if (typeof module !== "undefined") {
+	module.exports = translations;
+}
